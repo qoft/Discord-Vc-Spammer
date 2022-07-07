aa25e1f1a89ff5fe72d75121f894166e94784a20
qoft <63415260+Qoft@users.noreply.github.com>
2022-07-06 23:43:24 -0400

    h

--- main.py
+++ main.py
@@ -1,7 +1,53 @@
-import time, json, websocket,easygui, os; from concurrent.futures import ThreadPoolExecutor; print("Choose your tokens.txt"); tokenlist = open(easygui.fileopenbox(), 'r').read().splitlines() if os.name == 'nt' else open("tokens.txt", 'r').read().splitlines(); requests = 0; channel = int(input("Channel ID: ")); server = int(input("Server ID: ")); executor = ThreadPoolExecutor(max_workers=int(1000));os.system(f"title Total Tokens: {len(tokenlist)}")
-def printing():
-    while True: print("Total requests: " + str(requests), end="\r")
+from json import loads
+
+from time import sleep
+from json import dumps
+from websocket import WebSocket
+from concurrent.futures import ThreadPoolExecutor
+import easygui, os
+tokenlist = open(easygui.fileopenbox(), 'r').read().splitlines()
+channel = int(input("Channel ID: "))
+server = int(input("Server ID: "))
+deaf = input("Defean: (y/n) ")
+if deaf == "y":
+  deaf = True
+  if deaf == "n":
+    deaf = False
+mute = input("Mute: (y/n) ")
+if mute == "y":
+  mute = True
+  if mute == "n":
+    mute = False
+stream = input("Stream: (y/n) ")
+if stream == "y":
+  stream = True
+  if stream == "n":
+    stream = False
+video = input("Video: (y/n) ")
+if video == "y":
+  video = True
+  if video == "n":
+    video = False
+
+executor = ThreadPoolExecutor(max_workers=int(1000))
 def run(token):
-  while True: ws = websocket.WebSocket(); ws.connect("wss://gateway.discord.gg/?v=8&encoding=json"); ws.send(json.dumps({"op": 2,"d": {"token": token,"properties": {"$os": "windows","$browser": "Discord","$device": "desktop"}}})); ws.send(json.dumps({"op": 4,"d": {"guild_id": server,"channel_id": channel,"self_mute": False,"self_deaf": True, "self_stream?": True, "self_video": False}})); ws.send(json.dumps({"op": 18,"d": {"type": "guild","guild_id": server,"channel_id": channel,"preferred_region": "singapore"}})); ws.send(json.dumps({"op": 1,"d": None})); ws.close(); requests += 1; time.sleep(0.0347)
-for token in tokenlist: executor.submit(run, token)
-input("Spamming the server, press enter to exit")
+  while True:
+    ws = WebSocket()
+    ws.connect("wss://gateway.discord.gg/?v=8&encoding=json")
+    hello = loads(ws.recv())
+    heartbeat_interval = hello['d']['heartbeat_interval']
+    ws.send(dumps({"op": 2,"d": {"token": token,"properties": {"$os": "windows","$browser": "Discord","$device": "desktop"}}}))
+    ws.send(dumps({"op": 4,"d": {"guild_id": server,"channel_id": channel,"self_mute": mute,"self_deaf": deaf, "self_stream?": stream, "self_video": video}}))
+    ws.send(dumps({"op": 18,"d": {"type": "guild","guild_id": server,"channel_id": channel,"preferred_region": "singapore"}}))
+    ws.send(dumps({"op": 1,"d": None}))
+    ws.close()
+    sleep(0.1)
+    
+os.system(f"title Total Tokens: {len(tokenlist)}")
+i = 0
+for token in tokenlist:
+  executor.submit(run, token)
+  i+=1
+  print("[+] Joined voice channel")
+  sleep(0.01)
+yay = input("Enter to exit.")
